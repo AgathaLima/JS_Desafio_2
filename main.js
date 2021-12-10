aef9c2504f561830c9099125593ced9e72b74586
Agatha Lima <agatha,lima01@hotmail.com>
2021-12-10 18:29:36 -0300

    Criação da branch master

---
+++ main.js
@@ -0,0 +1,11 @@
+// const clienteTemp = {
+//     nome: 'Ágatha',
+//     email: 'agatha.lima01@hotmail.com',
+//     celular: '9999999-9999',
+//     cidade: 'Salvador'
+// }
+
+// const cadastraCliente = (cliente) => {
+//     localStorage.setItem('db_cliente', 'clienteTemp')
+// }
+
