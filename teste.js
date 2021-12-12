5e8309b4029894cbf6e2f4e4b8289c6f567d78ed
Agatha Lima <agatha.lima01@hotmail.com>
2021-12-11 23:30:56 -0300

    Criação do [create] do crud + alguns teste com os principais elementos da interação

--- teste.js
+++ teste.js
@@ -11,6 +11,31 @@ const tempCliente = {
 const getLocalStorage = () => JSON.parse(localStorage.getItem("db_client")) ?? []
 const setLocalStorage = (db_cliente) => localStorage.setItem("db_client", JSON.stringify(db_cliente)) 
 
+
+
+// Função CRUD - DELETE
+
+const deleteCliente = (index) => {
+    const db_cliente = readClient()
+    db_cliente.splice(index, 1)
+    setLocalStorage(db_cliente)
+}
+
+
+// Função CRUD - UPDATE
+
+const updateClient = (index, cliente) => {
+    const db_cliente = readClient()
+    db_cliente[index] = cliente
+    setLocalStorage(db_cliente)
+} 
+
+
+// Função CRUD - READ
+
+const readClient = () => getLocalStorage()
+
+
 // Função CRUD - CREATE
 
 const createClien = (cliente) => {
@@ -20,7 +45,7 @@ const createClien = (cliente) => {
 }  
 
 
-// Interação com a página
+// Interaçãpo com a página
 
 const saveClient = () => {
     if (validarCliente){
