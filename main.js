19eb045527cf1d7b68951af610e11d82989c1951
Ana Luiza Marques <analuizasmarques1@gmail.com>
2021-12-13 13:57:40 -0300

    Criando localStorage com o html já existente, criando read e update e iniciando a interação com o layout

--- main.js
+++ main.js
@@ -1,11 +1,74 @@
-// const clienteTemp = {
-//     nome: 'Ágatha',
-//     email: 'agatha.lima01@hotmail.com',
-//     celular: '9999999-9999',
-//     cidade: 'Salvador'
-// }
+const servicos = [
+  {
+    nome: 'Consultoria UX',
+    imagem: 'imagens/ilustra-ux.png',
+    descricao: 'Consequatur debitis ipsa numquam illum placeat quoddeleniti.'
+  },
+  {
+    nome: 'Marketing Digital',
+    imagem: 'imagens/ilustra-marketing.png',
+    descricao: ''
+  },
+  {
+    nome: 'Desenvolvimento Web',
+    imagem: 'imagens/ilustra-web.png',
+    descricao: 'Consequatur debitis ipsa numquam illum placeat quoddeleniti.'
+  }
+]
 
-// const cadastraCliente = (cliente) => {
-//     localStorage.setItem('db_cliente', 'clienteTemp')
-// }
+const getLocalStorage = () =>
+  JSON.parse(localStorage.getItem('db_servicos')) ?? []
 
+const setLocalStorage = dbServicos =>
+  localStorage.setItem('db_servicos', JSON.stringify(dbServicos))
+
+setLocalStorage(servicos)
+
+// Read and Update
+
+const readServicos = () => getLocalStorage()
+
+const updateServicos = (index, servicos) => {
+  const dbServicos = readServicos()
+  dbServicos[index] = servicos
+  setLocalStorage(dbServicos)
+}
+
+// Interação com o layout
+const validaCampos = () => {
+  return document.getElementById('form').reportValidity()
+}
+
+const salvaServico = () => {
+  if (validaCampos()) {
+    const servico = {
+      nome: document.getElementById('nome').value,
+      imagem: document.getElementById('imagem').value,
+      descricao: document.getElementById('descricao').value
+    }
+  }
+}
+
+const criaRow = servico => {
+  const novoRow = document.createElement('tr')
+  novoRow.innerHTML = `
+  <td>${servico.nome}</td>
+  <td>
+    <img src="${servico.imagem}" class="img-fluid" />
+  </td>
+  <td>
+    ${servico.descricao}
+  </td>
+  <td>
+    <button id="salvar" class="btn btn-secondary m-1">editar</button>
+    <button class="btn btn-danger m-1">excluir</button>
+  </td>
+  `
+}
+
+const updateTabela = () => {
+  const dbServicos = readServicos()
+  dbServicos.forEach(servico => {
+    criaRow(servico)
+  })
+}
